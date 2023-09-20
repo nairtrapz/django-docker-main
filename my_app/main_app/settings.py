1072a328097a37ffc47e845da363fb10d15d7ee4
Ali Asger Modi <aliasgermodi@Alis-MacBook-Pro.local>
2023-09-20 11:29:07 +0300

    setup models-admin-html

--- my_app/main_app/settings.py
+++ my_app/main_app/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-_$(qjq%het)gh!(&%w$-1_-7()h)ex4t)-jb!q3&dm8af*7sf@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -117,6 +118,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'sub_app', 'static')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'sub_app', 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- my_app/sub_app/views.py
+++ my_app/sub_app/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .forms import MyForms
 
 # Create your views here.
 def home(request):
-    return render(request, "home.html", {})
+    form = MyForms(request.POST, request.FILES)
+    if request.method == 'POST':
+        if form.is_valid():
+            form.save()
+    return render(request, "home.html", {"form":form})
