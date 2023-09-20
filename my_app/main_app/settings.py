2d9211eeab1b54860255868b8cee388a5a3c44ee
Ali Asger Modi <aliasgermodi@Alis-MacBook-Pro.local>
2023-09-20 15:30:46 +0300

    setup postgres with aws rds

--- my_app/main_app/settings.py
+++ my_app/main_app/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+import environ
+
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -77,8 +81,12 @@ WSGI_APPLICATION = 'main_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'myapp',
+        'USER': 'postgres',
+        'PASSWORD': 'ali12345',
+        "HOST": 'database-1.cyxt8banugtz.eu-north-1.rds.amazonaws.com',
+        "PORT": '5432'
     }
 }
 
